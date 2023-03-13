950268bc35ca8b2375b2c167b6ace8034499e82a
seolacarrano <seola6@gmail.com>
2023-03-13 12:07:28 -0400

    update homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,22 @@
+import './HomePage.css';
+import './ProjectsListPage';
+import ProjectsListPage from './ProjectsListPage';
+
 const HomePage = () => {
     return (
-        <>
-            <p className="bodyText"> This is a home page! </p>
-        </>
+        <div className = "homepage">
+           <div className="introduction">
+            <div className="text">
+                <h3>Seola Carrano</h3>
+                <h1>Software Engineer</h1>
+                <h2>developing software projects, identifying details</h2>
+            </div>
+            <div className="picture">
+            <img src="https://res.cloudinary.com/dqduwnrb1/image/upload/v1601408107/profile_ou0hwh.jpg" alt="profile picture"></img>
+            </div>
+           </div>
+           <ProjectsListPage />
+        </div>
 
     )
 }
